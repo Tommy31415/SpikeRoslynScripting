6438f33c7c06cf42012c6c2a71152638724ce641
Tomasz Pietraszko <tomasz.pietraszko@sabre.com>
2019-04-26 16:08:21 +0200

    check time of execution for basic C# Script using Roslyn

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace SpikeRoslynScripting
+{
+
+    public class ScriptHost
+    {
+        public ScriptHost(int number)
+        {
+            this.Number = number;
+
+        }
+        public int Number { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var scriptText = @"int Square(int number) {
+                return number*number;
+                }
+                Square(Number)";
+
+
+            var script = CSharpScript.Create<int>(scriptText, globalsType: typeof(ScriptHost));
+            ScriptRunner<int> runner = script.CreateDelegate();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();    
+            var val = runner( new ScriptHost(5)).Result;
+            watch.Stop();
+
+            Console.WriteLine(val);
+            Console.WriteLine($"Elapsed {watch.ElapsedMilliseconds} miliseconds");
+        }
+    }
+}
